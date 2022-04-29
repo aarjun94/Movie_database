197804b9876ab62b46de297d3687d32c396cd730
aarjun94 <103142559+aarjun94@users.noreply.github.com>
2022-04-28 20:58:56 -0400

    Add files via upload

--- tree.py
+++ tree.py
@@ -30,4 +30,10 @@ def create_tree():
     return tree
 data = create_tree()
 with open('tree.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(data, f, indent=4, sort_keys=True)
+
+
+
+# with open("tree.json", "r") as read_file:
+#     tree = json.load(read_file)
+#     PrettyJson = json.dumps(tree, indent=4, separators=(',', ': '), sort_keys=True)
